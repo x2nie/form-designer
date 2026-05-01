62fd5565c09d18598db5385207147099600d37f4
Fathony L <fathony@smart-leaders.net>
2026-05-01 12:39:04 +0700

    del dead code

--- src/components/form-designer.js
+++ src/components/form-designer.js
@@ -18,30 +18,3 @@ export class FormDesigner extends Component {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-FormDesigner.template0 = xml`
-<div class="window" style="width:300px; left:50px;">
-  <div class="title-bar">
-    <div class="title-bar-text" contenteditable="true">A Window With Stuff In It</div>
-    <div class="title-bar-controls">
-      <button aria-label="Minimize"></button>
-      <button aria-label="Maximize"></button>
-      <button aria-label="Close"></button>
-    </div>
-  </div>
-  <div class="window-body">
-    <p>There's so much room for activities!</p>
-  </div>
-</div>
-  
-`;
